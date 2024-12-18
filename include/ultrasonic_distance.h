95a574eac9bd82bc30f7d177cc86c07fa9d15fb6
Emil K <29043737+Cosmao@users.noreply.github.com>
2024-12-18 14:10:22 +0100

    task should be done

--- include/ultrasonic_distance.h
+++ include/ultrasonic_distance.h
@@ -5,6 +5,17 @@
 #include "soc/gpio_num.h"
 #include <stdint.h>
 
+#define numberOfMeasurements 3
+
+typedef struct {
+  gpio_num_t gpio_trigger;
+  gpio_num_t gpio_echo;
+  int8_t timeout_in_u_seconds;
+  int16_t measured_array[numberOfMeasurements];
+  int16_t average_measured;
+  uint8_t task_done;
+} distance_measurements;
+
 int16_t wait_for_echo(gpio_num_t gpio_pin, uint8_t timeout, uint8_t level);
 int16_t get_distance(int16_t u_seconds);
 esp_err_t init_distance_gpio(gpio_num_t gpio_trigger, gpio_num_t gpio_echo);
--- src/ultrasonic_distance.c
+++ src/ultrasonic_distance.c
@@ -1,8 +1,13 @@
 #include "ultrasonic_distance.h"
 #include "driver/gpio.h"
 #include "esp_err.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
 #include "hal/gpio_types.h"
 #include <rom/ets_sys.h>
+#include <time.h>
+
+static const char *TAG = "ULTRASONIC";
 
 /* Busy wait for the echo response */
 int16_t wait_for_echo(gpio_num_t gpio_pin, uint8_t timeout, uint8_t level) {
@@ -33,4 +38,30 @@ esp_err_t init_distance_gpio(gpio_num_t gpio_trigger, gpio_num_t gpio_echo) {
   }
   ret = gpio_set_direction(gpio_echo, GPIO_MODE_INPUT);
   return ret;
+}
+
+void measure_distance_task(void *pvParameters) {
+  distance_measurements *distance_struct =
+      (distance_measurements *)pvParameters;
+
+  ESP_ERROR_CHECK(init_distance_gpio(distance_struct->gpio_trigger,
+                                     distance_struct->gpio_echo));
+
+  uint16_t total_distance = 0;
+  uint8_t total_measurements = 0;
+  for (int16_t i = 0; i > numberOfMeasurements; i++) {
+    distance_struct->measured_array[i] = wait_for_echo(
+        distance_struct->gpio_echo, distance_struct->timeout_in_u_seconds, 0);
+    if (distance_struct->measured_array[i] == -1) {
+      ESP_LOGE(TAG, "Error in distance read loop %d", i);
+    } else {
+      total_measurements++;
+      total_distance += distance_struct->measured_array[i];
+    }
+  }
+
+  distance_struct->average_measured = total_distance / total_measurements;
+  ESP_LOGE(TAG, "Average distance: %dmm", distance_struct->average_measured);
+  distance_struct->task_done = 1;
+  vTaskDelete(NULL);
 }
