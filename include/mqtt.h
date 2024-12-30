b8b943136619c20f443f73766dbbbf69c93480aa
Emil K <29043737+Cosmao@users.noreply.github.com>
2024-12-30 13:53:57 +0100

    Added a task for wifi/mqtt for async

--- include/mqtt.h
+++ include/mqtt.h
@@ -4,5 +4,6 @@
 #include "mqtt_client.h"
 
 esp_mqtt_client_handle_t mqtt_start(void);
+void mqtt_task(void *pvParameter);
 
 #endif // !mqtt_h
--- src/mqtt.c
+++ src/mqtt.c
@@ -1,6 +1,8 @@
 #include "include/mqtt.h"
 #include "esp_event_base.h"
 #include "esp_log.h"
+#include "mqtt_client.h"
+#include "wifi.h"
 
 extern const uint8_t client_cert_pem_start[] asm("_binary_client_crt_start");
 extern const uint8_t client_cert_pem_end[] asm("_binary_client_crt_end");
@@ -103,4 +105,12 @@ esp_mqtt_client_handle_t mqtt_start(void) {
   snprintf(buff, buffSize, "{\"payload\":\"TestMessage\"}");
   esp_mqtt_client_enqueue(client, topic, buff, 0, 1, 0, false);
   return client;
+}
+
+void mqtt_task(void *pvParameter) {
+  esp_mqtt_client_handle_t mqtt_client = (esp_mqtt_client_handle_t)pvParameter;
+  if (wifi_init_station()) {
+    mqtt_client = mqtt_start();
+  }
+  return;
 }
