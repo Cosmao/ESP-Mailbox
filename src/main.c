54c99fa3bb80e193426628fb3bbb8c582b44a807
Emil K <29043737+Cosmao@users.noreply.github.com>
2024-12-30 14:10:20 +0100

    Mostly comments, todos and similar

--- src/main.c
+++ src/main.c
@@ -12,15 +12,16 @@
 #include "portmacro.h"
 #include "soc/gpio_num.h"
 #include "ultrasonic_distance.h"
-#include "wifi.h"
 #include <stdlib.h>
 #include <time.h>
 
 // TODO: give info about wifi/mqtt to main thread
 // Pass along the wakeup pin and sleep timer?
 // all the good logic
+// Put the wakeup time and similar in NVS for device shadows
 
 #define WAKEUP_PIN GPIO_NUM_7
+#define WAKEUP_TIME_SEC 20
 static char *TAG = "MAIN";
 
 void app_main(void) {
@@ -31,6 +32,10 @@ void app_main(void) {
     ret = nvs_flash_init();
   }
   ESP_ERROR_CHECK(ret);
+
+  TaskHandle_t distance_task_handle;
+  esp_mqtt_client_handle_t mqtt_client;
+  TaskHandle_t mqtt_task_handle;
 
   distance_measurements *distance_struct =
       (distance_measurements *)malloc(sizeof(distance_measurements));
@@ -45,20 +50,8 @@ void app_main(void) {
   distance_struct->gpio_trigger = GPIO_NUM_5;
   distance_struct->timeout_in_u_seconds = 5000;
 
-  TaskHandle_t distance_handle;
-  BaseType_t task_ret = xTaskCreate(measure_distance_task, "Distance-Task",
-                                    3048, distance_struct, 5, &distance_handle);
-
-  if (task_ret == errCOULD_NOT_ALLOCATE_REQUIRED_MEMORY) {
-    vTaskDelete(distance_handle);
-    ESP_LOGE(TAG, "Failed to create task, deleting");
-  }
-
-  esp_mqtt_client_handle_t mqtt_client;
-  TaskHandle_t mqtt_task_handle;
-
-  task_ret = xTaskCreate(&mqtt_task, "MQTT_Task", 4096, mqtt_client, 3,
-                         &mqtt_task_handle);
+  BaseType_t task_ret = xTaskCreate(&mqtt_task, "MQTT_Task", 4096, mqtt_client,
+                                    3, &mqtt_task_handle);
   if (task_ret == errCOULD_NOT_ALLOCATE_REQUIRED_MEMORY) {
     vTaskDelete(mqtt_task_handle);
     ESP_LOGE(TAG, "Could not allocate memory for mqtt");
@@ -66,19 +59,22 @@ void app_main(void) {
   }
 
   switch (get_wake_source()) {
+  case ESP_SLEEP_WAKEUP_UNDEFINED:
   case ESP_SLEEP_WAKEUP_TIMER: {
+    // TODO: Func for sending alive msg, enable time wakeup, enable rtc wake if
+    // closed
     break;
   }
   case ESP_SLEEP_WAKEUP_EXT0: {
-    break;
-  }
-  case ESP_SLEEP_WAKEUP_UNDEFINED: {
+    // TODO: Wait for close or timeout, send distance or timeout, enable wakes
     break;
   }
   default: {
+    // TODO: We should never end up here, send error msg
     break;
   }
   }
+  start_deep_sleep(mqtt_client);
 
   if (wait_for_low(WAKEUP_PIN, 30)) {
     ESP_LOGE(TAG, "Circuit open too long, disabling gpio wake");
@@ -86,6 +82,13 @@ void app_main(void) {
   } else {
     enable_rtc_io_wake(WAKEUP_PIN, 1);
     enable_timer_wake(20);
+  }
+
+  task_ret = xTaskCreate(measure_distance_task, "Distance-Task", 3048,
+                         distance_struct, 5, &distance_task_handle);
+  if (task_ret == errCOULD_NOT_ALLOCATE_REQUIRED_MEMORY) {
+    vTaskDelete(distance_task_handle);
+    ESP_LOGE(TAG, "Failed to create task, deleting");
   }
 
   while (distance_struct->task_done != 1)
