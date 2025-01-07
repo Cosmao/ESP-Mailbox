86bfd4fdfe0f77e56db8e83bb827acdca10446e6
Emil K <29043737+Cosmao@users.noreply.github.com>
2025-01-07 18:00:29 +0100

    Increased delay on ultrasonic

--- src/ultrasonic_distance.c
+++ src/ultrasonic_distance.c
@@ -97,7 +97,7 @@ void measure_distance_task(void *pvParameters) {
       ESP_LOGI(TAG, "Loop %d measured: %ldmm", i, distance);
     }
     /* delaying to let the sensor reset */
-    vTaskDelay(50 / portTICK_PERIOD_MS);
+    vTaskDelay(75 / portTICK_PERIOD_MS);
   }
 
   if (total_measurements == 0) {
