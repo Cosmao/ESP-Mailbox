73cd604370d1a36003452ba0a9725c4b2f76186c
Emil K <29043737+Cosmao@users.noreply.github.com>
2025-01-08 10:52:04 +0100

    Removed distance sensor from man

--- src/main.c
+++ src/main.c
@@ -1,14 +1,10 @@
 #include "deep_sleep.h"
 #include "esp_err.h"
-#include "esp_log.h"
-#include "esp_system.h"
 #include "mqtt.h"
 #include "mqtt_client.h"
 #include "nvs_flash.h"
 #include "sdkconfig.h"
 #include "ultrasonic_distance.h"
-#include <stdlib.h>
-#include <time.h>
 
 #define WAKEUP_PIN CONFIG_ESP_RTC_WAKEUP_PIN
 #define WAKEUP_TIME_HOURS CONFIG_ESP_WAKEUP_TIME_IN_HOURS
@@ -28,26 +24,13 @@ void app_main(void) {
   }
   ESP_ERROR_CHECK(ret);
 
-  distance_measurements *distance_struct =
-      (distance_measurements *)malloc(sizeof(distance_measurements));
-  if (distance_struct == NULL) {
-    ESP_LOGE(TAG, "Failed to malloc distance struct");
-    esp_restart();
-  }
-  distance_struct->task_done = 0;
-  distance_struct->gpio_echo = ECHO_PIN;
-  distance_struct->gpio_trigger = TRIG_PIN;
-  distance_struct->timeout_in_u_seconds = DISTANCE_TIMEOUT;
-
   esp_mqtt_client_handle_t mqtt_client = mqtt_enable();
   wake_actions action = WAKE_ACTION_NO_ACTION;
 
   enable_timer_wake(WAKEUP_TIME_HOURS * 60 * 60);
 
-  if (mqtt_client != NULL) {
-    action = handle_wake_source(WAKEUP_PIN);
-    handle_wake_actions(action, mqtt_client, distance_struct);
-  }
+  action = handle_wake_source(WAKEUP_PIN);
+  handle_wake_actions(action, mqtt_client);
 
   start_deep_sleep(mqtt_client);
 }
